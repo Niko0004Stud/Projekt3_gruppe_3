0293584ae0ee0dca7c387d632f82f55850a2760c
Niko0004Stud <niko0004@stud.ek.dk>
2026-05-02 13:23:02 +0200

    Det første udkast af controllermetode til at udskrive alle lejeaftaler

--- src/main/java/org/projekt3_gruppe_3/service/LejeaftaleService.java
+++ src/main/java/org/projekt3_gruppe_3/service/LejeaftaleService.java
@@ -1,7 +1,13 @@
 package org.projekt3_gruppe_3.service;
 
+import org.projekt3_gruppe_3.model.Lejeaftale;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LejeaftaleService {
+
+    //stub
+    public Lejeaftale getAllLejeaftaler(){
+        return null;
+    }
 }
