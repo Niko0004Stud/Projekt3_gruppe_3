09d6a876c7ed0493a4669f3822f19d47af1f52c4
Niko0004Stud <niko0004@stud.ek.dk>
2026-05-02 09:02:41 +0200

    Initial commit

---
+++ src/main/java/org/projekt3_gruppe_3/Projekt3Gruppe3Application.java
@@ -0,0 +1,13 @@
+package org.projekt3_gruppe_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt3Gruppe3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt3Gruppe3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/projekt3_gruppe_3/Projekt3Gruppe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projekt3_gruppe_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt3Gruppe3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
