de31330b49e883e15dc502e797abbc971d8bc18a
Estera Turcu <estu0002@stud.ek.dk>
2026-05-05 13:26:23 +0200

    TK04

---
+++ src/main/resources/BilabonnementDB.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS BilabonnementDB;
+
+CREATE DATABASE BilabonnementDB;
+
+USE BilabonnementDB;
+
+CREATE TABLE Kunde(
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    fornavn VARCHAR (50) NOT NULL,
+                    efternavn VARCHAR (50) NOT NULL,
+                    tlfNummer INTEGER NOT NULL,
+                    email VARCHAR(50) NOT NULL,
+                    adresse VARCHAR (100) NOT NULL,
+                    status VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE Bil(
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    vognnummer VARCHAR(50) NOT NULL,
+                    stelnummer VARCHAR(50) NOT NULL,
+                    mærke VARCHAR(50) NOT NULL,
+                    udsryrsniveau INTEGER NOT NULL,
+                    staalpris decimal NOT NULL,
+                    regAfgift decimal NOT NULL,
+                    co2Udledning decimal NOT NULL
+);
+
+
+CREATE TABLE Lejeaftale(
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            FOREIGN KEY (id) REFERENCES Bil(id),
+                            FOREIGN KEY (id) REFERENCES Kunde(id),
+                            startDate date NOT NULL,
+                            længdeDays INTEGER NOT NULL,
+                            prisKr decimal NOT NULL,
+                            skadeMatrixId INTEGER NOT NULL
+);
+
+CREATE TABLE Skade(
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    FOREIGN KEY (id) REFERENCES Lejeaftale(id),
+                    beskrivelse VARCHAR(100) NOT NULL,
+                    prispåskade decimal NOT NULL,
+                    registreringsDate date
+);
