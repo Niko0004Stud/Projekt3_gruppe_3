e2ae5846e4b6b483e33ebbabade2d52191e73ac6
Estera Turcu <estu0002@stud.ek.dk>
2026-05-06 11:26:09 +0200

    Task 1.3

--- src/main/java/org/projekt3_gruppe_3/model/Lejeaftale.java
+++ src/main/java/org/projekt3_gruppe_3/model/Lejeaftale.java
@@ -1,5 +1,6 @@
 package org.projekt3_gruppe_3.model;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class Lejeaftale {
@@ -10,6 +11,82 @@ public class Lejeaftale {
     private int skadeMatrixId;  //lidt i tvivl, der er snak
     private Date startDato;
     private int laengeDays;
-    private double prisKr;
+    private Date slutDato;
+    private BigDecimal prisKr;
+
+    public Lejeaftale(int id, int bilId, int kundeId, int skadeMatrixId, Date startDato, int laengeDays, Date slutDato, BigDecimal prisKr) {
+        this.id = id;
+        this.bilId = bilId;
+        this.kundeId = kundeId;
+        this.skadeMatrixId = skadeMatrixId;
+        this.startDato = startDato;
+        this.laengeDays = laengeDays;
+        this.slutDato = slutDato;
+        this.prisKr = prisKr;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBilId() {
+        return bilId;
+    }
+
+    public void setBilId(int bilId) {
+        this.bilId = bilId;
+    }
+
+    public int getKundeId() {
+        return kundeId;
+    }
+
+    public void setKundeId(int kundeId) {
+        this.kundeId = kundeId;
+    }
+
+    public int getSkadeMatrixId() {
+        return skadeMatrixId;
+    }
+
+    public void setSkadeMatrixId(int skadeMatrixId) {
+        this.skadeMatrixId = skadeMatrixId;
+    }
+
+    public Date getStartDato() {
+        return startDato;
+    }
+
+    public void setStartDato(Date startDato) {
+        this.startDato = startDato;
+    }
+
+    public int getLaengeDays() {
+        return laengeDays;
+    }
+
+    public void setLaengeDays(int laengeDays) {
+        this.laengeDays = laengeDays;
+    }
+
+    public Date getSlutDato() {
+        return slutDato;
+    }
+
+    public void setSlutDato(Date slutDato) {
+        this.slutDato = slutDato;
+    }
+
+    public BigDecimal getPrisKr() {
+        return prisKr;
+    }
+
+    public void setPrisKr(BigDecimal prisKr) {
+        this.prisKr = prisKr;
+    }
 
 }
