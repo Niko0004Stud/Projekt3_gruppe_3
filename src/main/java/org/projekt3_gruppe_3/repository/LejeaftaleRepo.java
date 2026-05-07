a5d4186de427e54637b7b889e71c94bbf51839a5
Estera Turcu <estu0002@stud.ek.dk>
2026-05-07 10:51:40 +0200

    Main.test

--- src/main/java/org/projekt3_gruppe_3/repository/LejeaftaleRepo.java
+++ src/main/java/org/projekt3_gruppe_3/repository/LejeaftaleRepo.java
@@ -13,31 +13,27 @@ import java.time.LocalDate;
 
 @Repository
 public class LejeaftaleRepo {
-    //1.3 metode C from CRUD: CREATE
 
 @Autowired
 DataSource dataSource;
 
 public void createLejeaftale(Lejeaftale lejeaftale){
-    String sql="INSERT INTO lejeaftale (Lejeaftaleid, BilId, KundeId,skadeMatrixId, startDato, laengdeDays, slutDato, prisKr) " +
-            "VALUES(?,?,?,?,?,?,?,?)";
-
+    String sql="INSERT INTO lejeaftale (BilId, KundeId,skadeMatrixId, startDato, laengdeDays, slutDato, prisKr) " +
+                "VALUES(?,?,?,?,?,?,?)";
 
     try(Connection connection=dataSource.getConnection();
         PreparedStatement statement=connection.prepareStatement(sql)) {
-        statement.setInt(1, lejeaftale.getId());
-        statement.setInt(2, lejeaftale.getBilId());
-        statement.setInt(3, lejeaftale.getKundeId());
-        statement.setInt(4, lejeaftale.getSkadeMatrixId());
-        statement.setDate(5, (LocalDate) lejeaftale.getStartDato());
-        statement.setInt(6, lejeaftale.getLaengeDays());
-        statement.setDate(7, (LocalDate) lejeaftale.getSlutDato());
-        statement.setBigDecimal(8, lejeaftale.getPrisKr());
+        statement.setInt(1, lejeaftale.getBilId());
+        statement.setInt(2, lejeaftale.getKundeId());
+        statement.setInt(3, lejeaftale.getSkadeMatrixId());
+        statement.setDate(4, Date.valueOf((LocalDate) lejeaftale.getStartDato()));
+        statement.setInt(5, lejeaftale.getLaengeDays());
+        statement.setDate(6, Date.valueOf((LocalDate) lejeaftale.getSlutDato()));
+        statement.setBigDecimal(7, lejeaftale.getPrisKr());
         statement.executeUpdate();
-    }catch (SQLException e){
-        e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
-}
-
 
 }
