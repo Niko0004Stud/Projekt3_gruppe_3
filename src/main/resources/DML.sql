0ee721ba16982f059bbdf451dd8e294659014166
Estera Turcu <estu0002@stud.ek.dk>
2026-05-13 20:12:40 +0200

    dummy data

--- src/main/resources/DML.sql
+++ src/main/resources/DML.sql
@@ -1,13 +1,16 @@
-insert into Bil (vognnummer, stelnummer, modelId, udstyrsniveau,
+INSERT INTO Bil (vognnummer, stelnummer, modelId, udstyrsniveau,
                  staalpris, regAfgift, co2Udledning)
 VALUES  (6, 3, 6, 7, 100,
          40.09, 10.60);
 
-insert into SkadeMatrix (skadeId, bilId, omkostninger,registreringsDate)
-VALUES  (1,4, 5555, 2010-05-04);
+INSERT INTO SkadeMatrix (skadeId, bilId, omkostninger,registreringsDate)
+VALUES  (1,4, 5555, 2026-05-13);
 
 INSERT INTO User(username, password, sysrole)
 VALUES ('testAdmin', 'testpassword', 'admin');
 
 INSERT INTO User(username, password, sysrole)
 VALUES ('testDR', 'testpassword', 'dataregistrerer');
+
+INSERT INTO  LaKvittering(skadeMatrixId, startDate, slutDate, totalPrisKR, type)
+VALUES (2, 2026-05-13, 2026-05-13, 500.5, 'noget')
