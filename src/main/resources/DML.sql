d5dbdb8965675ffa514b8ccf9c4c184766b88dee
Estera Turcu <estu0002@stud.ek.dk>
2026-05-13 11:01:35 +0200

    dummy data rettet

--- src/main/resources/DML.sql
+++ src/main/resources/DML.sql
@@ -1,11 +1,13 @@
-insert into Bil (vognnummer, stelnummer, maerke, udstyrsniveau,
+insert into Bil (vognnummer, stelnummer, modelId, udstyrsniveau,
                  staalpris, regAfgift, co2Udledning)
 VALUES  ('1','2', 'opel', 5, 500,
-         400, 200)
+         400, 200);
 
-insert into SkadeMatrix (beskrivelse, prisPaaSkade, registreringsDate)
-VALUES  ('big dmg', '555555', 09-)
+insert into SkadeMatrix (skadeId, bilId, omkostninger,registreringsDate)
+VALUES  ('big dmg', '555555', 2020-06-09);
+
 INSERT INTO User(username, password, sysrole)
 VALUES ('testAdmin', 'testpassword', 'admin');
+
 INSERT INTO User(username, password, sysrole)
 VALUES ('testDR', 'testpassword', 'dataregistrerer');
